bb745bd0970d91e89bc311537b50eb26e5d77cb4
Karim Joseph, Jr <info@karimjosephjr.com>
2018-09-11 18:52:00 -0600

    Add very basic Space class.

--- chess.py
+++ chess.py
@@ -9,10 +9,22 @@ class Board:
         for i in range(8):
             self.board.append([])
             for j in range(8):
-                self.board[i].append("space")
+                self.board[i].append(Space(i, j))
 
     def __str__(self):
         return str(self.board)
+
+
+class Space:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.color = None
+        self.piece = None
+        if (self.x + 2) % 2 == 0 and (self.y + 2) % 2 == 0:
+            self.color = "white"
+        else:
+            self.color = "black"
 
 
 some_board = Board()
