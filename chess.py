62955ad3c394ce6ca70fa40644309f079f3d3186
Karim Joseph, Jr <info@karimjosephjr.com>
2018-09-11 20:13:24 -0600

    Add printing board with colorama.

--- chess.py
+++ chess.py
@@ -6,13 +6,21 @@ init()
 class Board:
     def __init__(self):
         self.board = []
-        for i in range(8):
+        for y in range(8):
             self.board.append([])
-            for j in range(8):
-                self.board[i].append(Space(i, j))
+            for x in range(8):
+                self.board[y].append(Space(x, y))
 
     def __str__(self):
-        return str(self.board)
+        board_array = [[str(Back.WHITE + Fore.BLACK + ' K ' if x.color == "white"
+                            else Back.BLACK + ' K ') for x in y] for y in self.board]
+        board_array2 = []
+        for y in board_array:
+            board_array2.append(''.join(y) + Style.RESET_ALL)
+        return Back.BLACK + '\n'.join(board_array2)
+
+    def get_space(self, x, y):
+        return self.board[y][x]
 
 
 class Space:
@@ -23,8 +31,13 @@ class Space:
         self.piece = None
         if (self.x + 2) % 2 == 0 and (self.y + 2) % 2 == 0:
             self.color = "white"
+        elif (self.x + 2) % 2 != 0 and (self.y + 2) % 2 != 0:
+            self.color = "white"
         else:
             self.color = "black"
+
+    def __str__(self):
+        return self.color
 
 
 some_board = Board()
