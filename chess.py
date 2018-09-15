c7c24a4461d573caf5e181c22475fad83bd8b62d
Karim Joseph, Jr <info@karimjosephjr.com>
2018-09-15 17:49:49 -0600

    Update view, adjust coordinate reference, and cleanup.

--- chess.py
+++ chess.py
@@ -11,30 +11,34 @@ class Board:
     @staticmethod
     def make_board():
         board = []
-        for y in range(8):
+        for x in range(8):
             board.append([])
-            for x in range(8):
-                board[y].append(Space(x, y))
+            for y in range(8):
+                board[x].append(Space(x, y))
         return board
 
     @staticmethod
     def make_grid():
         grid = {}
         for coord_2, letter in enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']):
-            for coord_1, num in enumerate(range(7, -1, -1)):
+            for coord_1, num in enumerate(range(8, 0, -1)):
                 grid[f'{letter}{num}'] = (coord_1, coord_2)
         return grid
 
+    def board_str(self):
+        letters = ['  ', ' a ', ' b ', ' c ', ' d ', ' e ', ' f ', ' g ', ' h ', '\n']
+        array_of_array_of_str_spaces = [[str(x) for x in y] for y in self.board]
+        array_of_str_spaces = []
+        for row, label in enumerate(range(8, 0, -1)):
+            array_of_str_spaces.append(str(label) + ' ' + ''.join(array_of_array_of_str_spaces[row]) + Style.RESET_ALL)
+        return ''.join(letters) + '\n'.join(array_of_str_spaces)
+
     def __str__(self):
-        board_array = [[str(x) for x in y] for y in self.board]
-        board_array2 = []
-        for y in board_array:
-            board_array2.append(''.join(y) + Style.RESET_ALL)
-        return Back.BLACK + '\n'.join(board_array2)
+        return self.board_str()
 
     def update_board(self, start, end):
-        self.board[end[1]][end[0]].piece = self.board[start[1]][start[0]].piece
-        self.board[start[1]][start[0]].piece = None
+        self.board[end[0]][end[0]].piece = self.board[start[0]][start[1]].piece
+        self.board[start[0]][start[1]].piece = None
 
 
 class Space:
@@ -43,17 +47,21 @@ class Space:
         self.piece = None
         self.whiteIsThreatened = None
         self.blackIsThreatened = None
-        self.color = None
+        self.color = self.set_space_color(x, y)
+
+    @staticmethod
+    def set_space_color(x, y):
         if (x + 2) % 2 == 0 and (y + 2) % 2 == 0:
-            self.color = Back.WHITE
+            color = Back.WHITE
         elif (x + 2) % 2 != 0 and (y + 2) % 2 != 0:
-            self.color = Back.WHITE
+            color = Back.WHITE
         else:
-            self.color = Back.BLACK
+            color = Back.BLACK
+        return color
 
     def __str__(self):
         if self.piece:
-            return self.color + self.piece
+            return self.color + str(self.piece)
         else:
             return self.color + '   '
 
@@ -61,7 +69,7 @@ class Space:
 # test cases
 some_board = Board()
 print(some_board.grid)
-# print(some_board)
+print(some_board)
 # print("\n\n\n")
 #
 # some_board.board[1][0].piece = Style.BRIGHT + " K "
