5ccdaa59a6c1bd566aa8767d6ca139c54150548a
David Fuller <dragonslayerdavide@gmail.com>
2018-09-16 14:21:43 -0600

    Create Player.py
    Class Player contains the make_a_move method

---
+++ Player.py
@@ -0,0 +1,68 @@
+#define Player class
+
+class Player:
+
+	def __init__(self, name="Default", color="White"):
+		self.name = name
+		self.color = color
+	
+	def make_a_move(self,board):
+		'''
+		Prompt the player to select a piece that they wish to move by providing the space that piece occupies
+		Then prompt the player to select where they would like to move that piece to
+		Update the board after validation 
+		'''
+		
+		#board.get_coords(string) returns either None or a tuple
+		#board.update_board(start,end) is expecting two tuples (coord of piece that is going to move)(coord of destination)
+		
+		valid_piece = None
+		while not valid_piece:
+			piece_space = input("Select the piece that you wish to move by providing the space it currently occupies: ")
+			piece_tup = board.get_coords(piece_space)
+			if piece_tup != None and board.board[piece_tup[0]][piece_tup[1]].piece != None:
+				valid_piece = board.board[piece_tup[0]][piece_tup[1]].piece
+				
+		valid_moves = valid_piece.move_options(piece_tup)
+		move_choice = None
+		while move_choice not in valid_moves:
+			potential_move = input("Select the space that you would like to move your piece to: ")
+			move_choice = board.get_coords(potential_move)
+			
+		board.update_board(piece_tup,move_choice)
+
+
+# Help visualize board:
+# [
+
+# [0,1,2,3,4,5,6,7], 8
+# [0,1,2,3,4,5,6,7], 7
+# [0,1,2,3,4,5,6,7], 6
+# [0,1,2,3,4,5,6,7], 5 
+# [0,1,2,3,4,5,6,7], 4
+# [0,1,2,3,4,5,6,7], 3
+# [0,1,2,3,4,5,6,7], 2
+# [0,1,2,3,4,5,6,7]  1
+#  A B C D E F G H
+# ]
+# Top left is [0][0] {'A8':(0,0)}, bottom right is [7][7] {'H1':(7,7)}		[Number][Letter]
+
+# Letter	Index
+# A		    0	
+# B		    1
+# C		    2
+# D		    3
+# E		    4
+# F		    5
+# G		    6
+# H		    7
+
+# Number	Index
+# 1		    7	
+# 2		    6
+# 3		    5
+# 4		    4
+# 5		    3
+# 6		    2
+# 7		    1
+# 8		    0
