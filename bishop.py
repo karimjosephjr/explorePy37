52a51f31b63b4557e20a6aeb660719a6b2171e2c
Karim Joseph, Jr <info@karimjosephjr.com>
2018-09-20 21:56:10 -0600

    Clean up Biship class for merge.

--- bishop.py
+++ bishop.py
@@ -1,10 +1,19 @@
+from colorama import init
+from colorama import Fore, Style
+init()
+
+
 class Bishop:
 
     def __init(self, color="White"):
         self.color = color
 
     def __str__(self):
-        return ' B '
+        if self.color.lower()[0] == "w":
+            piece_color = Fore.WHITE + Style.BRIGHT
+        else:
+            piece_color = Fore.BLUE + Style.BRIGHT
+        return piece_color + ' B ' + Style.RESET_ALL
 
     @staticmethod
     def check_direction(position, direction):
@@ -20,12 +29,12 @@ class Bishop:
         return options
 
     def move_options(self, position):
-        '''
+        """"
         position - a tuple that represents where the king is on the board
         possible_moves is a list of positions that the king could move to without concern for size of the board
         the for loop retains elements from the possible_moves list that are valid given an 8x8 board
         returns a list that can be used for Player.validate_move
-        '''
+        """
 
         valid_moves = list()
         valid_moves += self.check_direction(position, (1, 1))
